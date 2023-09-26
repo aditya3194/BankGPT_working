ada01cad71e734781f9bf4a4b3134cd75565580b
aditya3194 <adityamahajan6040@gmail.com>
2023-09-26 16:57:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ with st.form('myform', clear_on_submit=True):
 			    with st.spinner("Thinking..."):
 				    response = generate_response(prompt) 
 				    st.write(response) 
-	    message = {"role": "assistant", "content": response}
-            st.session_state.messages.append(message)
+	    	    message = {"role": "assistant", "content": response}
+                    st.session_state.messages.append(message)
 
 
