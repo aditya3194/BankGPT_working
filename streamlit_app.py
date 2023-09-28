e19a304db24d8ec5dbed60a063739903cee5535a
aditya3194 <adityamahajan6040@gmail.com>
2023-09-28 09:31:00 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ import cohere
 st.set_page_config(page_title="India MoF-WIP")
 
 with st.sidebar:
-	st.title('India MoF')
-	st.text('c6pobgap7gKlXOuU29e97W3Q0A2mJhg01hfbWwlJ')
-	openai_api_key = st.text_input('Cohere API Key')
-	st.button('Proceed!')
-	if not (openai_api_key):
-	    st.warning('Please enter your credentials!', icon='⚠️')
-	else:
-	    st.success('Proceed to entering your prompt message!', icon='👉')
+		st.title('India MoF')
+		st.text('c6pobgap7gKlXOuU29e97W3Q0A2mJhg01hfbWwlJ')
+		openai_api_key = st.text_input('Cohere API Key')
+		st.button('Proceed!')
+		if not (openai_api_key):
+			st.warning('Please enter your credentials!', icon='⚠️')
+		else:
+			st.success('Proceed to entering your prompt message!', icon='👉')
 
 co = cohere.Client(openai_api_key)
 
@@ -30,26 +30,28 @@ for message in st.session_state.messages:
 chat_history = []
 
 def generate_response(prompt_input):
-	response = co.chat(
-	prompt_input, 
-	model="command-nightly", 
-	chat_history= chat_history,
-	temperature=0.3
-    	)
-	user_message = {"user_name": "User", "text": prompt_input}
-	bot_message = {"user_name": "Chatbot", "text": response.text}
-	
-	chat_history.append(user_message)
-	chat_history.append(bot_message)
-	return response.text
+		response = co.chat(
+		prompt_input, 
+		model="command-nightly", 
+		chat_history= chat_history,
+		temperature=0.3
+		)
+		user_message = {"user_name": "User", "text": prompt_input}
+		bot_message = {"user_name": "Chatbot", "text": response.text}
+		
+
+		chat_history.append(user_message)
+		chat_history.append(bot_message)
+		print(chat_history)
+		return response.text
 
 # User-provided prompt
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
-	    
-# st.text( st.session_state.messages)
+
+st.text( st.session_state.messages)
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
